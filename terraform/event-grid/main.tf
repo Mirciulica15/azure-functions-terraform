85bc84c1e6c2e73ad4d6bdb78c231f03a2faf84d
mircea.talu <talumircea13@gmail.com>
2023-10-11 15:17:27 +0300

    Add only Resource Write Success as event type for event subscription

--- terraform/event-grid/main.tf
+++ terraform/event-grid/main.tf
@@ -10,6 +10,10 @@ resource "azurerm_eventgrid_event_subscription" "main" {
   name  = "evgs-${var.workload}-${var.environment}-${var.region}"
   scope = data.azurerm_subscription.current.id
 
+  included_event_types = [
+    "Microsoft.Resources.ResourceWriteSuccess"
+  ]
+
   azure_function_endpoint {
     function_id = "${data.azurerm_windows_function_app.main.id}/functions/tagwithcreator"
   }
