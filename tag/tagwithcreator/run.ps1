a143943d12a844ee5a948d1058ccea264c276491
mircea.talu <talumircea13@gmail.com>
2023-10-11 18:00:36 +0300

    Add correct hash table for hidden tags

--- tag/tagwithcreator/run.ps1
+++ tag/tagwithcreator/run.ps1
@@ -49,7 +49,10 @@ if ($tags) {
             if (!($tags.properties.TagsProperty.ContainsKey('Creator')) ) {
                 Update-AzTag -ResourceId $resourceId -Operation Merge -Tag $newTag | Out-Null
                 Update-AzTag -ResourceId $resourceId -Tag @{CreatedDate=$(Get-Date)} -Operation Merge
-                Update-AzTag -ResourceId $resourceId -Tag "hidden-DeployedBy: terraform" -Operation Merge
+                $hiddenTags = @{
+                    "hidden-DeployedBy" = "terraform"
+                }
+                Update-AzTag -ResourceId $resourceId -Tag $hiddenTags -Operation Merge
                 Write-Host "Added Creator and CreatedDate tags with user: $caller"
             }
             else {
